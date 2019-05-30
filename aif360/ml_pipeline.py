3602c6bede1880861f471f8841e3513a4c9412a2
Jesse Bartola <jrbartola@gmail.com>
2019-05-30 17:20:56 -0400

    Cleaned up metric evaluation and other parmaeters

--- aif360/ml_pipeline.py
+++ aif360/ml_pipeline.py
@@ -1,8 +1,8 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-from metrics import ClassificationMetric
-from algorithms.postprocessing import CalibratedEqOddsPostprocessing
+from aif360.metrics import ClassificationMetric
+from aif360.algorithms.postprocessing import CalibratedEqOddsPostprocessing
 
 class MLPipeline(object):
 
@@ -23,28 +23,33 @@ class MLPipeline(object):
         self.unprivileged = unprivileged
         self.preprocessor = preprocessor
         self.postprocessor = postprocessor
+        self.dataset_train = []
+        self.dataset_test = []
+        self.test_predictions = []
 
 
-    def fit(self, dataset, n=0.3, threshold=0.5, feature_scaling=False, metrics=[]):
+    def fit(self, dataset, test_frac=0.3, threshold=0.5, feature_scaling=False):
         """
         Trains our model on the dataset.
 
         Args:
             dataset (aif360.datasets.StructuredDataset): An instance of a structured dataset
-            n (float): A real number between 0 and 1 denoting the % of the dataset to be used as test data
+            test_frac (float): A real number between 0 and 1 denoting the % of the dataset to be used as test data
             threshold (float): A real number between 0 and 1 denoting the threshold of acceptable class imbalance
-            metrics (list[DatasetMetric]): A list of AIF360 metric class objects
         """
 
-        if n < 0 or n > 1:
-            raise ValueError("Parameter n must be between 0 and 1")
+        if test_frac < 0 or test_frac > 1:
+            raise ValueError("Parameter test_frac must be between 0 and 1")
 
-        dataset_train, dataset_test = dataset.split([1-n], shuffle=False)
+        dataset_train, dataset_test = dataset.split([1-test_frac], shuffle=False)
 
         # If a preprocessing function was supplied, transform the data first
         if self.preprocessor:
             dataset_train = self.preprocessor.fit_transform(dataset_train)
             dataset_test = self.preprocessor.fit_transform(dataset_test)
+
+        self.dataset_train = dataset_train
+        self.dataset_test = dataset_test
         
         X_train = dataset_train.features
         y_train = dataset_train.labels.ravel()
@@ -75,26 +80,35 @@ class MLPipeline(object):
             dataset_test_pred = self.postprocessor.fit(dataset_test, dataset_test_pred) \
                                                   .predict(dataset_test_pred)
 
-        # Evaluate our metrics
-        for i, metric in enumerate(metrics):
+        self.test_predictions = dataset_test_pred
 
-            # Try instantiating the metric using only the dataset argument. If it's the classification metric, then we'll fall through to the
-            # except statement since that metric requires an extra argument.
-            try:
-                curr_metric = metric(dataset_test, unprivileged_groups=self.unprivileged, privileged_groups=self.privileged)
+    
+    def evaluate(self, metric):
+        """
+        Evaluates an AIF360 metric against the trained model
 
-                # TODO: Change this to something user-specified
-                print("METRIC ({})".format(i+1))
-                print("========================")
-                print(curr_metric.mean_difference(), "\n")
-            except TypeError:
-                curr_metric = metric(dataset_test, dataset_test_pred, unprivileged_groups=self.unprivileged, privileged_groups=self.privileged)
+        Args:
+            metric (aif360.metrics.Metric): An AIF360 metric class
+        Raises:
+            AttributeError: If a model has not been trained, an AttributeError will be raised
+        Returns:
+            float: A float denoting the performance of the specified metric on the trained model
+        """
 
-                # TODO: Change this to something user-specified
-                print("METRIC ({})".format(i+1))
-                print("========================")
-                print(curr_metric.mean_difference(), "\n")
-                
+        import re
+        
+        if not self.dataset_train:
+            raise AttributeError("A model must be fit before evaluating a metric")
+
+        # Try instantiating the metric using only the dataset argument. If it's the classification metric, then we'll fall through to the
+        # except statement since that metric requires an extra argument.
+        try:
+            curr_metric = metric(self.dataset_test, unprivileged_groups=self.unprivileged, privileged_groups=self.privileged)
+        except TypeError:
+            curr_metric = metric(self.dataset_test, self.test_predictions, unprivileged_groups=self.unprivileged, privileged_groups=self.privileged)
+        
+        # TODO: Figure out a way to swtich this out for other methods of evaluation
+        return curr_metric.mean_difference()
 
 
 if __name__ == '__main__':
@@ -103,15 +117,21 @@ if __name__ == '__main__':
     from sklearn.linear_model import LogisticRegression
     
     # Example. Load the adult dataset, train with LogisticRegression and postprocess with EqOddsPostProcessing 
-    dataset = AdultDataset()
-    privileged_groups = [{'race': 1}] # white = 1
-    unprivileged_groups = [{'race': 0}]
-    postprocessor = CalibratedEqOddsPostprocessing(privileged_groups=privileged_groups, unprivileged_groups=unprivileged_groups)
+    DATASET = AdultDataset()
+    PRIVILIGED_GROUPS = [{'race': 1}] # white = 1
+    UNPRIVILIGED_GROUPS = [{'race': 0}]
+    METRICS = [ClassificationMetric, BinaryLabelDatasetMetric]
+
+    # Instantiate our pre/postprocessing algorithms
+    postprocessor = CalibratedEqOddsPostprocessing(privileged_groups=PRIVILIGED_GROUPS, unprivileged_groups=UNPRIVILIGED_GROUPS)
     
-    # Create our pipeline object, fit and evaluate our metrics.
-    # TODO: Perhaps the metric evaluation could be moved outside of the fit function.
-    mlp = MLPipeline(LogisticRegression(), privileged_groups, unprivileged_groups, postprocessor=postprocessor)
-    mlp.fit(dataset, metrics=[ClassificationMetric, BinaryLabelDatasetMetric])
+    # Create our pipeline object and fit our model
+    mlp = MLPipeline(LogisticRegression(), PRIVILIGED_GROUPS, UNPRIVILIGED_GROUPS, postprocessor=postprocessor)
+    mlp.fit(DATASET)
+    
+    # Evaluate our metrics
+    for metric in METRICS:
+        mlp.evaluate(metric)
 
 
 
